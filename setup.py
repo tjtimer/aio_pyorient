e04f7b0f9b51a0e104fe52951e82c3c4cb112afb
tjtimer <tjtimer@gmail.com>
2018-02-14 15:22:14 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 
-setup(name='aio_pyorient',
-      version='0.0.1',
-      author='Tim "tjtimer" Jedro <tjtimer@gmail.com>',
-      description='asyncio OrientDB native client library',
-      long_description=open('README.rst').read(),
-      license='LICENSE',
-      packages = [
-          'aio_pyorient',
-          'aio_pyorient.messages',
-          'aio_pyorient.ogm',
+setup(
+    name='aio_pyorient',
+    version='0.0.1',
+    url='https://github.com/tjtimer/aio_pyorient',
+    author='Tim "tjtimer" Jedro',
+    author_email='tjtimer@gmail.com',
+    description='async OrientDB client library',
+    long_description=open('README.rst').read(),
+    license='LICENSE',
+    packages = [
+        'aio_pyorient',
+        'aio_pyorient.messages',
+        'aio_pyorient.ogm',
+    ],
+    install_requires=[
+        'pytest',
+        'pytest-aiohttp',
+        'pyorient_native'
     ]
-      )
+)
