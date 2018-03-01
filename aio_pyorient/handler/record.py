3d2e11c25ea84ceba51ce68f714d1f451af0a8d4
tjtimer <tjtimer@gmail.com>
2018-03-01 21:27:07 +0100

    removed handler/record.py

--- aio_pyorient/handler/record.py
+++
@@ -1,7 +0,0 @@
-from collections import namedtuple
-
-from aio_pyorient.handler.base import BaseHandler
-
-RecordCreateResponse = namedtuple("RecordCreateResponse", "")
-class Create(BaseHandler):
-    pass
