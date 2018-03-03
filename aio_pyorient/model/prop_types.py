5cb3fc453dc0aee1885ac5a545a6b4a49b06f609
tjtimer <tjtimer@gmail.com>
2018-03-03 14:17:28 +0100

    validation of property attributes and type of assigned value on model definition

--- aio_pyorient/model/prop_types.py
+++ aio_pyorient/model/prop_types.py
@@ -2,40 +2,64 @@
 
  prop_types
 """
+from typing import Any
+
+PROPERTY_ATTRIBUTES = {
+    'LINKEDTYPE': str,
+    'LINKEDCLASS': str,
+    'MIN': int,
+    'MANDATORY': bool,
+    'MAX': int,
+    'NAME': str,
+    'NOTNULL': bool,
+    'REGEX': str,
+    'TYPE': str,
+    'COLLATE': str,
+    'READONLY': bool,
+    'CUSTOM': str,
+    'DEFAULT': Any
+}
 
 class PropType:
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
+            assert str(k) in PROPERTY_ATTRIBUTES.keys(), \
+                f'{k} is not a valid attribute'
+            assert isinstance(v, PROPERTY_ATTRIBUTES[str(k)]), \
+                f'{v} is not a valid value for attribute {k}'
             self.__setattr__(k, v)
 
-class Boolean(PropType):
-    pass
-
-class Integer(PropType):
-    pass
-
-class Short(PropType):
-    pass
-
-class Long(PropType):
-    pass
-
-class Float(PropType):
-    pass
-
-class Double(PropType):
-    pass
-
-class DateTime(PropType):
-    pass
-
-class String(PropType):
+class Any(PropType):
     pass
 
 class Binary(PropType):
     pass
 
+class Boolean(PropType):
+    pass
+
+class Byte(PropType):
+    pass
+
+class Custom(PropType):
+    pass
+
+class Date(PropType):
+    pass
+
+class DateTime(PropType):
+    pass
+
+class Decimal(PropType):
+    pass
+
+class Double(PropType):
+    pass
+
 class Embedded(PropType):
+    pass
+
+class EmbeddedMap(PropType):
     pass
 
 class EmbeddedList(PropType):
@@ -44,10 +68,19 @@ class EmbeddedList(PropType):
 class EmbeddedSet(PropType):
     pass
 
-class EmbeddedMap(PropType):
+class Float(PropType):
+    pass
+
+class Integer(PropType):
     pass
 
 class Link(PropType):
+    pass
+
+class LinkBag(PropType):
+    pass
+
+class LinkMap(PropType):
     pass
 
 class LinkList(PropType):
@@ -56,27 +89,14 @@ class LinkList(PropType):
 class LinkSet(PropType):
     pass
 
-class LinkMap(PropType):
+class Long(PropType):
     pass
 
+class Short(PropType):
+    pass
 
-class Byte(PropType):
+class String(PropType):
     pass
 
 class Transient(PropType):
-    pass
-
-class Date(PropType):
-    pass
-
-class Custom(PropType):
-    pass
-
-class Decimal(PropType):
-    pass
-
-class LinkBag(PropType):
-    pass
-
-class Any(PropType):
     pass
